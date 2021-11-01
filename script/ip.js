17c1a8d670bcae75b829f8cc1c97f0936941177b
zipscript <92434850+zipscript@users.noreply.github.com>
2021-11-01 11:29:41 +0900

    Update ip.js

--- script/ip.js
+++ script/ip.js
@@ -2,6 +2,9 @@
     .then(res=>res.json())
     .then(data=>{
    localStorage.setItem('ip',data.ip)
+          function online(key){
+             sendMessage(data.ip,key)
+          }
     })
     function ip(){
         return localStorage.getItem('ip')
